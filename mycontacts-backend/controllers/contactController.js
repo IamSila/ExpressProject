30950a218be1febe07ce074b706c8547a2c16b4d
IamSila <silamulingi@gmail.com>
2025-06-10 12:39:19 +0300

    still on the contactController ... updated

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -1,42 +1,48 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc get all the contacts
 //@route GET /api/contacts
 //@access public
 
-const getContacts = (req, res) => {
-    res.status(200).json({message: "fetching all contacts from the user"});
-}
+const getContacts = asyncHandler(async (req, res) => {
+ res.status(200).json({message: "fetching all contacts from the user"});
+});
 
 //@desc get a single contact
 //@route GET /api/contacts/:id
 //@access public
 
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
     res.status(200).json({message: `fetching contact for ${req.params.id}`});
-}
+});
 
 //@desc create new contacts
 //@route POST /api/contacts
 //@access public
 
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
+        res.status(404);
+        throw new Error("all options must be available");
+    }
     res.status(201).json({message: "Creating a user account"});
-}
+});
 
 //@desc create new contacts
 //@route PUT /api/contacts/:id
 //@access public
 
-const updateContact = (req, res) => {
-    res.status(200).json({message: `Modifying data for user ${req.params.id}`});
-}
+const updateContact = asyncHandler(async (req, res) => { res.status(200).json({message: `Modifying data for user ${req.params.id}`});
+});
 
 //@desc delete contact
 //@route DELETE /api/contacts/:id
 //@access public
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Deleting account for user ${req.params.id}`});
-}
+});
 
 module.exports = { 
     getContact,
--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
 
 const port = 5000;
 
+app.use(express.json());
+app.use(errorHandler);
 // routes
 app.use("/api/contacts", require("./routes/contactRoutes"));
 
